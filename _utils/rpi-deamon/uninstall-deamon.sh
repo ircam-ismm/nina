9b4b6b549fbb80227308bc9b6721333dc8360a02
b-ma <benjamin.matuszewski@gmail.com>
2023-09-15 16:43:08 +0200

    chore: fix filename

--- _utils/rpi-deamon/uninstall-deamon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-service="nina-daemon.service"
-
-echo "Stop \"$service\""
-sudo systemctl stop "$service"
-echo "Disable \"$service\""
-sudo systemctl disable "$service"
-sudo systemctl daemon-reload
